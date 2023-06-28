e297d165f8cef68a8634739fc9cba2842f9d0ce5
SAS <ahmetsahin@Ahmet-MacBook-Pro.local>
2023-06-28 07:07:47 +0100

    v3

--- src/test/java/deneme2.java
+++ src/test/java/deneme2.java
@@ -1,9 +1,5 @@
 public class deneme2 {
     public static void main(String[] args) {
         System.out.println("Merhaba");
-        System.out.println("Dunya");
     }
 }
-
-// git restore --> Tamamen working space i siler
-//git stash  --> hafizaya alir sonradan bulabiliriz.
